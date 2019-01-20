05bc0644c91a40bc6b89efa50a8c72c4aad504d6
cppdy <cppdy813@163.com>
2019-01-21 00:37:56 +0800

    mybatis开启log4j+给包下所有类起别名

--- src/com/jeff/test/Test.java
+++ src/com/jeff/test/Test.java
@@ -13,31 +13,31 @@ import com.jeff.entity.User;
 
 public class Test {
 
-	public static void main(String[] args) throws Exception {
-		InputStream is = Resources.getResourceAsStream("mybatis.xml");
-		// 使用工厂设计模式
-		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
-		// 生产SqlSession
-		SqlSession session = factory.openSession();
+    public static void main(String[] args) throws Exception {
+        InputStream is = Resources.getResourceAsStream("mybatis.xml");
+        // 使用工厂设计模式
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
+        // 生产SqlSession
+        SqlSession session = factory.openSession();
 
-		// --------------------------适用于查询结果都需要遍历的需求-----------------------
-		List<User> list = session.selectList("a.b.selAll");
-		System.out.println("适用于查询结果都需要遍历的需求");
-		for (User flower : list) {
-			System.out.println(flower.toString());
-		}
+        // --------------------------适用于查询结果都需要遍历的需求-----------------------
+        List<User> list = session.selectList("a.b.selAll");
+        System.out.println("适用于查询结果都需要遍历的需求");
+        for (User flower : list) {
+            System.out.println(flower.toString());
+        }
 
-		// --------------------------适用于返回结果只是变量或一行数据时-----------------------
-		User user = session.selectOne("a.b.selById", 1);
-		System.out.println("适用于返回结果只是变量或一行数据时");
-		System.out.println(user);
+        // --------------------------适用于返回结果只是变量或一行数据时-----------------------
+        User user = session.selectOne("a.b.selById", 1);
+        System.out.println("适用于返回结果只是变量或一行数据时");
+        System.out.println(user);
 
-		// --------------------------适用于需要在查询结果中通过某列的值取到这行数据的需求-----------------------
-		Map<Object, Object> map = session.selectMap("a.b.selAll", "id");
-		System.out.println("适用于需要在查询结果中通过某列的值取到这行数据的需求");
-		System.out.println(map);
+        // --------------------------适用于需要在查询结果中通过某列的值取到这行数据的需求-----------------------
+        Map<Object, Object> map = session.selectMap("a.b.selAll", "id");
+        System.out.println("适用于需要在查询结果中通过某列的值取到这行数据的需求");
+        System.out.println(map);
 
-		session.close();
-	}
+        session.close();
+    }
 
 }
